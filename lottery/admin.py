7e2526f3b4a349187ecf285c5d551b9ccc6dd59f
xcFelix <xcFelix@qq.com>
2017-07-09 14:09:22 +0800

    register lottery.models

--- lottery/admin.py
+++ lottery/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from lottery.models import Prize, UserByPrize, Result
 
-# Register your models here.
+
+admin.site.register(Prize)
+admin.site.register(UserByPrize)
+admin.site.register(Result)
