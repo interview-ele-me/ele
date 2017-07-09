a7373c048e0ff5bfe670f287cb63b3459a1f56a0
xcFelix <xcFelix@qq.com>
2017-07-09 13:51:59 +0800

    init lottery app

--- ele/settings.py
+++ ele/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'ele.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ele',
+        'USER': 'root',
+        'PASSWORD': 'ele123ME',
+        'HOST': 'rm-uf653vi02y2o6o370o.mysql.rds.aliyuncs.com',
+        'PORT': '3306',
     }
 }
 
