5d95eeb1ecb381c45ae833062d978e5aeb5f4e4a
xcFelix <xcFelix@qq.com>
2017-07-09 14:03:54 +0800

    add lottery/models.py

--- lottery/models.py
+++ lottery/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Prize(models.Model):
+    name = models.CharField(max_length=100, default='')
+    total_num = models.IntegerField(default=0)
+    lucky_num = models.IntegerField(default=0)
+
+
+class UserByPrize(models.Model):
+    user = models.ForeignKey(User)
+    prize = models.ForeignKey(Prize)
+    lucky_num = models.IntegerField(default=0)
+    create_time = models.DateField(auto_now=True)
+
+
+class Result(models.Model):
+    user = models.ForeignKey(User)
+    prize = models.ForeignKey(Prize)
+    lucky_num = models.IntegerField(default=0)
